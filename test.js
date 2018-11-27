0bf2ed41a1f9543062eb633a8bee408b57034d11
JustF <folkerthoogenraad@hotmail.nl>
2018-11-27 11:46:54 +0100

    Added correction model

--- test.js
+++ test.js
@@ -5,18 +5,22 @@ var v = {
         {
             "type" : "question",
             "text" : "Beschrijf wat een tabel is in een database.",
+            "answer" : "Een datatype of entiteit (met verschillende kolommen) in een database",
             "points" : 1
         },
         {
             "type" : "question",
             "text" : "Beschrijf wat een record is in een database.",
+            "answer" : "Een rij in een tabel (met informatie)",
             "points" : 1
         },
         {
             "type" : "question",
             "text" : "Beschrijf wat SQL is",
+            "answer" : "Een (database) vraagtaal",
             "points" : 2
         },
+
         {
             "type" : "info",
             "text" : "De school database bestaat uit 4 tabellen: leerlingen, cijfers, toetsen en vakken. Elke toets heeft een toetsnummer. Als een leerling een toets heeft gemaakt staat er een rij in de cijfers tabel met het bijbehorende leerlingnummer en toetsnummer. Elke toets hoort bij een vak. In het onderstaande strokendiagram wordt de structuur van de database weergegeven.<br><img width=\"400\" src=\"img/database.png\"></img>"
@@ -29,34 +33,40 @@ var v = {
         {
             "type" : "question",
             "text" : "Geef de voor- en achternamen van alle leerlingen die in Doetinchem wonen, gesorteerd op achternaam",
+            "answer" : "<code>SELECT voornaam, naam FROM leerlingen WHERE woonplaats = \"Doetinchem\" ORDER BY achternaam</code>",
             "points" : 2
         },
         {
             "type" : "question",
             "text" : "Geef alle unieke woonplaatsen",
+            "answer" : "<code>SELECT DISTINCT woonplaats FROM leerlingen</code>",
             "points" : 1
         },
 
         {
             "type" : "question",
             "text" : "Geef de leerlingnummers van alle leerlingen die geen herkansing hebben gemaakt voor de \"Atmosfeer\" toets",
+            "answer" : "<code>SELECT leerlingnr FROM toetsen, cijfers WHERE toetsen.toetsnr = cijfers.toetsnr AND toetsnaam = \"Atmosfeer\" AND herkansing IS NULL</code>",
             "points" : 2
         },
         {
             "type" : "question",
             "text" : "Geef de voor- en achternaam van alle leerlingen die ooit een herkansing hebben gemaakt",
+            "answer" : "<code>SELECT DISTINCT naam, achternaam FROM leerlingen, cijfers WHERE leerlingen.leerlingnr = cijfers.leerlingnr AND NOT herkansing IS NULL</code>",
             "points" : 2
         },
 
         {
             "type" : "question",
             "text" : "Geef het gemiddelde cijfer van alle toetsen (zonder herkansingen)",
-            "points" : 2
+            "answer" : "<code>SELECT AVG(cijfer) FROM cijfers</code>",
+            "points" : 1
         },
         {
             "type" : "question",
-            "text" : "Geef het aantal herkansingen",
-            "points" : 1
+            "text" : "Geef het laagste cijfer per toets (zonder herkansingen)",
+            "answer" : "<code>SELECT MIN(cijfer), toetsnr FROM cijfers GROUP BY toetsnr</code>",
+            "points" : 2
         }
     ]
 };
@@ -87,6 +97,16 @@ function generateQuestion(question, index){
         div("col11", 
             b("", "Vraag " + index + ". ") + 
             question.text
+        )
+    );
+}
+
+function generateQuestionAnswer(question, index){
+    return div("question",
+        div("col1", question.points + "pnt") + 
+        div("col11", 
+            b("", "Vraag " + index + ". ") + 
+            question.answer
         )
     );
 }
@@ -133,6 +153,28 @@ function generateTest(test){
     return str;
 }
 
+function generateAnswers(test){
+    var str = "";
+
+    str += h1("", test.title);
+    str += div("intro",
+        p("", "Elke fout is een punt minder van de vraag. Er worden geen negatieve punten toegekend.") + 
+        p("", "Totale punten: " + getTotalPoints(test))
+    );
+
+    var index = 0;
+
+    test.questions.forEach(element => {
+        if(element.type === "question"){
+            index++;
+            str += generateQuestionAnswer(element, index);
+        }
+    });
+
+    return str;
+}
+
 window.onload = function(){
-    document.body.innerHTML = generateTest(v);
+    //document.body.innerHTML = generateTest(v);
+    document.body.innerHTML = generateAnswers(v);
 }
